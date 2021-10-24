cbc603648c0bcf18697dc2209b2fde1a398ec8d3
Yuri N. Matveev <ynmatveev@yandex.ru>
2021-10-24 19:38:58 +0300

    change vps for deploy

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -25,14 +25,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    '130.193.57.232', 'localhost', 'ynm-project.online', 'www.ynm-project.online'
-]
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split()
 
 # Application definition
 
